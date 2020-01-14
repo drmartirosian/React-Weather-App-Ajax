6ece92611006f3092855961d3f1aac7563f6410d
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-14 16:21:30 -0600

    Map renders

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,18 +5,28 @@ import Map from '../../components/Map/Map';
 import { getCurrentLatLng } from '../../services/geolocation';
 
 class App extends Component {
+  // Initialize state
+  state = {
+    lat: null,
+    lng: null
+  };
 
   // Add this method
   async componentDidMount() {
     // Destructure the object returned from getCurrentLatLng()
     const {lat, lng} = await getCurrentLatLng();
+    // Replace the console.log with the following
+  this.setState({lat, lng});
     console.log(lat, lng);
   }
 
   render() {
     return (
       <div className='App'>
-        <Map />
+        <Map
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
         <header className='App-header'>REACT WEATHER</header>
       </div>
     );
